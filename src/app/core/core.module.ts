8b2115de738d8b8f014113e0409bba3f87169728
Moonsub Um <moonsub.um@gmail.com>
2021-12-12 22:06:59 +0900

    app: 기본 스타일, 문구, 라우팅 구현

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,12 +2,20 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { TipsComponent } from './components/tips.component';
 import { IntroComponent } from './containers/intro.component';
 import { QuestionsComponent } from './containers/questions.component';
 import { ResultComponent } from './containers/result.component';
+import { TextSeqDirective } from './services/text-seq.directive';
 
 @NgModule({
-  declarations: [QuestionsComponent, IntroComponent, ResultComponent],
+  declarations: [
+    QuestionsComponent,
+    IntroComponent,
+    ResultComponent,
+    TextSeqDirective,
+    TipsComponent,
+  ],
   imports: [
     CommonModule,
     FormsModule,
@@ -16,7 +24,19 @@ import { ResultComponent } from './containers/result.component';
       {
         path: '',
         pathMatch: 'full',
+        component: IntroComponent,
+      },
+      {
+        path: 'begin',
         component: QuestionsComponent,
+      },
+      {
+        path: 'result',
+        component: ResultComponent,
+      },
+      {
+        path: 'tips',
+        component: TipsComponent,
       },
     ]),
   ],
---
+++ src/app/core/services/text-seq.directive.ts
@@ -0,0 +1,42 @@
+import {
+  Directive,
+  ElementRef,
+  HostBinding,
+  Input,
+  OnInit,
+} from '@angular/core';
+import { interval, timer } from 'rxjs';
+import { switchMapTo, takeWhile } from 'rxjs/operators';
+
+@Directive({
+  selector: '[textSeq]',
+})
+export class TextSeqDirective implements OnInit {
+  constructor(private elref: ElementRef) {
+    this.el = this.elref.nativeElement;
+  }
+  @Input() interval = 100; // ms
+  @Input() delay = 0;
+  @HostBinding('style.display') display?: string;
+  el!: HTMLElement;
+  prevDisplay: string = 'block';
+
+  ngOnInit() {
+    this.prevDisplay = this.display!;
+    this.display = 'none';
+    this.el.style.display = 'none';
+    const text = Array.from(this.el.innerText.trim());
+    this.el.innerText = '';
+    this.display = this.prevDisplay;
+    timer(this.delay)
+      .pipe(
+        switchMapTo(interval(this.interval)),
+        takeWhile((idx) => idx < text.length)
+      )
+      .subscribe({
+        next: (idx) => {
+          this.el.innerHTML += text[idx];
+        },
+      });
+  }
+}
